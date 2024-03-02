24beeb2470f6a3dccf30652b4698ce11fc95db38
Shivam Khattar <56852615+iamkhattar@users.noreply.github.com>
2024-03-02 02:16:07 +0000

    Brewfile cleanup

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # Clone dotfiles
 git clone https://github.com/iamkhattar/dotfiles.git $HOME/.dotfiles
 
+# Symlink dotfiles
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -21,16 +23,20 @@ brew bundle --file $HOME/.dotfiles/Brewfile
 
 # Check for Oh My Zsh and install if we don't have it
 if test ! $(which omz); then
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
-# Install binaries
-
-
-# Install nvim
-
 # Install nvchad
+rm -rf ~/.config/nvim
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
-# Install dotfiles
+# Create Projects directory
+mkdir -p $HOME/projects
+mkdir -p $HOME/projects/personal
+mkdir -p $HOME/projects/work
 
-#
+# Clone key repositories
+
+# Verify installation
+chmod +x $HOME/.dotfiles/verify.sh
+$HOME/.dotfiles/verify.sh
